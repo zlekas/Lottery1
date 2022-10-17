3bc947e8d4e3685d0aa8d2f91b7161f491e9821e
zlekas <114683079+zlekas@users.noreply.github.com>
2022-10-17 16:19:18 -0700

    Add files via upload

---
+++ lottery/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'permit mention object blush junior surprise devote always notice six quote fold',
+  'https://goerli.infura.io/v3/4cdc9ad0eb7d4e9badf605325c39509d'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface)
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
